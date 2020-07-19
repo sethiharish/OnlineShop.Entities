fd6e73bd0b531e7f5959b3caa2288e23b3464057
Harish Sethi <sethi.harish@gmail.com>
2020-07-19 20:11:19 +1000

    Add Banner, Category and Pie entities

---
+++ OnlineShop.Entities/Banner.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineShop.Entities
+{
+    public class Banner
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
